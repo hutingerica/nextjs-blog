59d4724efd68555f7932e0c77b795d07e05d1e16
hutingerica <hutingstudio@gmail.com>
2021-04-06 18:06:03 +0100

    learn next.js

---
+++ pages/posts/[id].js
@@ -0,0 +1,41 @@
+import Layout, { siteTitle } from '../../components/layout'
+import utilStyles from '../../styles/utils.module.css'
+
+import Head from 'next/head'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import Date from '../../components/date'
+
+
+export default function Post({ postData }) {
+    return (
+    <Layout>
+        <Head>
+            <title>{postData.title} | { siteTitle } </title>
+        </Head>
+        <article>
+            <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+            <div className={utilStyles.lightText}>
+                <Date dateString={postData.date} />
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
+    </Layout>
+    )
+  }
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds()
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({ params }) {
+    const postData = await getPostData(params.id)
+    return {
+      props: {
+        postData
+      }
+    }
+  }
